fab8610dc2ad859c4adc56f0c3320e0de58faac5
bismakhan21 <bismakhan2022@gmail.com>
2023-10-10 17:01:12 +0500

    Assignment Accomplish

---
+++ app.js
@@ -0,0 +1,147 @@
+// Chapter 46, 48 (Events)
+
+// Q1-----------
+
+// function message() {
+//     alert("Hello user");
+// }
+
+// Q2------------- 
+
+// var elementToHide = document.getElementById("elementToHide");
+
+//         document.addEventListener("click", function(event)  {
+//             if (event.target !== elementToHide) {
+//                 elementToHide.style.display = "none";
+//             }
+
+//         }  )
+
+// Q3-----------done
+
+// Q4------------- 
+
+// function paraScrolling() {
+//   document.getElementById("demo").innerHTML = div;
+// }
+
+// paraScrolling();
+
+// Q5-----------
+
+// var audio1 = new Audio("./audio/mouse-click-153941.mp3");
+
+// function sound(audio1) {
+//        return audio1;
+// }
+// sound(audio1);
+
+// Q6------------done
+
+// Q7-------------
+
+// function message() {
+//        alert("Hello World");
+//    }
+
+// Q8--------------done
+
+// Q9--------------
+// function play() {
+       
+//     var audio = new Audio("./audio/mouse-click-153941.mp3");
+    
+// }
+
+// play();
+
+// Q10------------
+
+// function message() {
+//        alert("This is logo image");
+// }
+
+// Q11-------------done
+
+//Q12---------
+
+// var myWindow;
+
+// function openWindow() {
+//   myWindow = window.open(" ", " ","width=200,height=100");
+// }
+
+// function closeWindow() {
+//   myWindow.close();
+// }
+
+
+// Chapter 49, 50 (Reading and Setting field values)
+
+// Q1---------------check
+
+// function check() {
+//        alert(name.value);
+// }
+
+// check();
+
+// Q2-------------
+
+// function check() {
+     
+//        console.log("click on check");
+
+// }
+
+// check(this);
+
+// Q3-------------
+
+// function selectedValue() {
+//        var selectBox = document.getElementById("selected");
+//         var selectValue = selectBox.value;
+//        console.log("Selected value: " + selectValue);
+//    }
+
+//    selectedValue();
+
+// Q4------------
+
+// Q5-----------
+
+// function marital() {
+//        var married = false;
+//        var maritalStatus = document.getElementById("status");
+            
+//             if (!married) {
+//                 maritalStatus.value = "available";
+//             }
+//             return true;
+
+// }
+
+// marital();
+
+// Q6-------------
+
+// function validFormFill() {
+//        var name = document.getElementById("name").value;
+//        var email = document.getElementById("email").value;
+//        return true;
+//    }
+
+//    validFormFill();
+
+
+// Q7------------
+
+// function validateForm() {
+//        var name = document.getElementById("name").value;
+//        var email = document.getElementById("email").value;
+//        var password = document.getElementById("password").value;
+      
+//        return true;
+//    }
+
+//    validateForm();
